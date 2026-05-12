ee6c08d8ac3d7d9ac02249ca421028acf939161d
정명호 <jmh0856@gmail.com>
2026-05-12 10:57:34 +0900

    fix: ALLOWED_HOSTS EC2 IP 추가

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS: list = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS: list = ["localhost", "0.0.0.0", "127.0.0.1", "54.180.114.107"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
